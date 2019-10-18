522687b8e3e5fdcfba4d7cb4204576d281596a83
20172210016 <20172210016@pc-106344.passofundo.ifsul.local>
2019-10-18 20:39:39 -0300

    Realizando o comit

---
+++ src/novoprojeto/Classe2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package novoprojeto;
+
+/**
+ *
+ * @author 20172210016
+ */
+public class Classe2 {
+    
+}
